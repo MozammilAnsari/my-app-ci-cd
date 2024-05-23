a87ae5ccde1f2f5e591e8a0c5b16853a40e97f62
MozammilAnsari <93940544+MozammilAnsari@users.noreply.github.com>
2024-05-23 12:28:44 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-echo "Pulling image from ecr"
+# Define variables
+AWS_REGION="us-east-1"
+ECR_REGISTRY="3381492249871.dkr.ecr.us-east-1.amazonaws.com"
+ECR_REPOSITORY="my-app"
+IMAGE_TAG="latest"
 
-docker pull 381492249871.dkr.ecr.us-east-1.amazonaws.com/my-app:latest
+# Get the login command from ECR and execute it directly
+echo "Logging in to ECR"
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 381492249871.dkr.ecr.us-east-1.amazonaws.com
 
+# Pull the Docker image from ECR
+echo "Pulling image from ECR"
+docker pull $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
 
 # Run the Docker image as a container
-echo "Run the docker image"
-
-docker run -d -p 5000:5000 381492249871.dkr.ecr.us-east-1.amazonaws.com/my-app:latest
+echo "Running the Docker image"
+docker run -d -p 5000:5000 $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
 
